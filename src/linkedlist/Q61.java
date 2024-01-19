e951ac85b539723f17ed0b52a6b717677257d4fe
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-01-19 18:21:09 -0500

    2014-01-19-2

---
+++ src/linkedlist/Q61.java
@@ -0,0 +1,37 @@
+package linkedlist;
+
+import model.ListNode;
+
+public class Q61 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0) return head;
+
+        //calculate the length of the listNode; and the tail of the linked list
+        int length = 1;
+        ListNode tail = head;
+        while( tail.next != null){
+            length++;
+            tail = tail.next;
+        }
+
+        //make a circle
+        tail.next = head;
+
+        //calculate the steps after circle
+        k = k%length;
+
+        //find the newTail: the listNode before K
+        ListNode newTail = head;//initialize the newTail
+        for(int i = 1; i <= length-k-1 ; i++){//记得要走的步数是长度-k -1 从末尾往前走
+            newTail = newTail.next;
+        }
+
+        //the new head will be newTail.next
+        ListNode newHead = newTail.next;
+
+        //break the circle ;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
