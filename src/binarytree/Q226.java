1e7899aee6961bfb637af60d2f98e113228f884d
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-01-31 10:50:50 -0500

    2024-01-31

--- src/binarytree/Q226.java
+++ src/binarytree/Q226.java
@@ -8,6 +8,7 @@ public class Q226 {
         TreeNode left = root.left;//先保留左边的值
         root.left = invertTree(root.right);//改变左边的值
         root.right = invertTree(left);//将之前保留的值放到右边去
+        ////不能直接root.right = invertTree(root.left);因为root.left已经改成right了
         return root;
     }
 }
