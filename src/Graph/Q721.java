81a296810513d57c63bd162f9f965f5acc705098
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-04-06 17:17:44 -0400

    Update Q721.java

--- src/Graph/Q721.java
+++ src/Graph/Q721.java
@@ -6,6 +6,80 @@ import java.util.List;
 import java.util.Map;
 
 public class Q721 {
+    class Solution2 {
+        public List<List<String>> accountsMerge(List<List<String>> accounts) {
+            //step1: hashmap<mail,index> -- use Unionfind.union the index
+            Unionfind uf = new Unionfind(accounts.size());
+            HashMap<String, Integer> mailIndexMap = new HashMap<>();
+
+            //traversal the account
+            for(int i = 0; i < accounts.size();i++){
+                for(int j = 1; j < accounts.get(i).size(); j++){
+                    //union the index
+                    String mail = accounts.get(i).get(j);
+                    //错误写法：//contansKey；contiansKey
+                    if(mailIndexMap.containsKey(mail)){
+                        uf.Union(i,mailIndexMap.get(mail));
+                    }else{
+                        mailIndexMap.put(mail,i);
+                    }
+                }
+            }
+
+            //step2: hashmap<index,mial> -- use Unionfind.find the root node index
+            HashMap<Integer, List<String>> indexMailMap = new HashMap<>();
+            //traversal all the mail //错误写法keyset
+            for(String mail : mailIndexMap.keySet()){
+                int index = mailIndexMap.get(mail);
+                int rootIndex = uf.find(index);
+                indexMailMap.putIfAbsent(rootIndex, new ArrayList<>());
+                indexMailMap.get(rootIndex).add(mail);
+            }
+
+            //step3: print res
+            List<List<String>> res = new ArrayList<>();
+            //traversal the map get the list of mail
+            for(int i : indexMailMap.keySet()){
+                List<String> listOfMail = indexMailMap.get(i);
+                String name = accounts.get(i).get(0);
+                listOfMail.sort((a,b) -> a.compareTo(b));
+                listOfMail.add(0,name);
+                res.add(listOfMail);
+            }
+            return res;
+        }
+
+        class Unionfind{
+            int[] parents;
+            //n
+            public Unionfind(int n){
+                this.parents = new int[n];
+                for(int i = 0; i < n ; i++){
+                    parents[i] = i;
+                }
+            }
+
+            //union the index
+            public void Union(int i,int j){
+                //错误写法：int p = parents[i],q = parents[j];
+                int p = find(i),q = find(j);
+                if(p == q){
+                    return;
+                }
+                parents[p]= q;
+            }
+
+            //find the root node index
+            public Integer find(int i){
+                while(parents[i] != i ){
+                    parents[i] = parents[parents[i]];
+                    i = parents[i];
+                }
+                return i;
+            }
+        }
+
+    }
     class Solution {
         public List<List<String>> accountsMerge(List<List<String>> accounts) {
             UnionFind uf = new UnionFind(accounts.size());
