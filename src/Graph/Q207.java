1cc2afac2d0eff63834aa43114684b4079ed2236
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-05-02 17:04:19 -0400

    Update Q207.java

--- src/Graph/Q207.java
+++ src/Graph/Q207.java
@@ -1,11 +1,54 @@
 package Graph;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Q207 {
+    class Solution2 {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            // 创建邻接表，用 Map 表示，值为 Set
+            Map<Integer, Set<Integer>> adjacencyList = new HashMap<>();
+            for (int i = 0; i < numCourses; i++) {
+                adjacencyList.put(i, new HashSet<>());
+            }
+
+            // 统计每门课程的入度
+            int[] indegree = new int[numCourses];
+
+            // 构建邻接表和入度数组
+            for (int[] prerequisite : prerequisites) {
+                int course = prerequisite[0];
+                int prerequisiteCourse = prerequisite[1];
+                adjacencyList.get(prerequisiteCourse).add(course);
+                indegree[course]++;
+            }
+
+            // 将入度为0的课程加入队列作为起始点
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < numCourses; i++) {
+                if (indegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+
+            // 记录已学习的课程数量
+            int learnedCourses = 0;
+            while (!queue.isEmpty()) {
+                int course = queue.poll();
+                learnedCourses++;
+                // 将当前课程的后继课程的入度减一，若入度为0，则加入队列
+                for (int nextCourse : adjacencyList.get(course)) {
+                    indegree[nextCourse]--;
+                    if (indegree[nextCourse] == 0) {
+                        queue.offer(nextCourse);
+                    }
+                }
+            }
+
+            // 若所有课程都能学习，则返回true；否则返回false
+            return learnedCourses == numCourses;
+        }
+    }
+
     class Solution {
         public boolean canFinish(int numCourses, int[][] prerequisites) {
 
