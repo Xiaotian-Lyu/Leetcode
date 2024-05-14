5981e112c9a1dd6bbc037268db32969729c63f26
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-05-14 16:33:01 -0700

    Update Q210.java

--- src/Graph/Q210.java
+++ src/Graph/Q210.java
@@ -3,6 +3,60 @@ package Graph;
 import java.util.*;
 
 public class Q210 {
+    class SolutionSelf {
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            //adjacencyMap - course precourse
+            //错误写法：Map<Integer,set<Integer>>
+            Map<Integer,Set<Integer>> adjacencyMap = new HashMap<>();
+
+            //indegree - each course's precourses number
+            int[] indegree = new int[numCourses];
+
+            //put all data into the map
+            for(int[] prerequisite : prerequisites){
+                int course = prerequisite[0];
+                int preCourse = prerequisite[1];
+                adjacencyMap.putIfAbsent(preCourse,new HashSet<>());//错：putIfAbscent
+                adjacencyMap.get(preCourse).add(course);
+                indegree[course]++;
+            }
+
+            //queue - store : canTakeCourse    indegree[course] == 0
+            Queue<Integer> queue = new LinkedList<>();
+            for(int i = 0; i < indegree.length; i++){
+                if(indegree[i] == 0){
+                    //错误写法：queue.add(indegree[i]);
+                    queue.add(i);
+                }
+            }
+
+            //poll the canTakeCourse - finishedCourse
+            //result
+            int finishedCourse = 0;
+            int[] res = new int[numCourses];
+            while(!queue.isEmpty()){
+                int canTakeCourse = queue.poll();
+                //错误写法：res.add(canTakeCourse);???
+                res[finishedCourse++] = canTakeCourse;
+
+                if(adjacencyMap.containsKey(canTakeCourse)){//containskey
+
+                    for(int nextCourse : adjacencyMap.get(canTakeCourse)){
+                        indegree[nextCourse]--;//重要
+                        if(indegree[nextCourse] == 0){
+                            queue.add(nextCourse);
+                        }
+                    }
+                }
+
+            }
+
+            //check with the numCourses = finishedCourse?
+            //return the result
+            //错误写法：return numCourses == finishedCourse ? res : new int[];
+            return numCourses == finishedCourse ? res : new int[0];
+        }
+    }
     class Solution {
         public int[] findOrder(int numCourses, int[][] prerequisites) {
             Map<Integer, Set<Integer>> adjacencyMap = new HashMap<>();
