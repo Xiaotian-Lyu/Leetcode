b90e2d12366acf15febe16a9f734a5838b212926
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-05-26 22:46:51 -0700

    Update Q3.java

--- src/SlidingWindow/Q3.java
+++ src/SlidingWindow/Q3.java
@@ -4,6 +4,28 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Q3 {
+    class Solution2 {
+        public int lengthOfLongestSubstring(String s) {
+            Set<Character> set = new HashSet<>();
+            int i = 0, j = 0, res = 0;
+
+            while(j < s.length()){
+                //错误写法：Char c
+                char c = s.charAt(j);
+
+                while(set.contains(c)){
+                    set.remove(s.charAt(i));
+                    i++;//不能忘记！！！
+                }
+                set.add(c);
+                //错误写法：Math.Max
+                res = Math.max(res, set.size());
+                j++;//不能忘记！！！
+            }
+            return res;
+        }
+
+    }
     class Solution {
         public int lengthOfLongestSubstring(String s) {
             Set<Character> set = new HashSet<>();
@@ -20,5 +42,6 @@ public class Q3 {
             }
             return res;
         }
+
     }
 }
