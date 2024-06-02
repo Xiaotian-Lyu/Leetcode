b694cd2fe83ca1df42477b38033a041f58587b16
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-01 23:43:34 -0700

    Update Q133.java

--- src/Graph/Q133.java
+++ src/Graph/Q133.java
@@ -17,9 +17,11 @@ public class Q133 {
     }
 
 
+    //递归调用：代码使用递归方法helper来实现节点的克隆。
+    //每次调用helper时，都会先克隆当前节点，然后递归克隆其所有邻居节点。
+    //这种递归方式本质上就是深度优先搜索。
     private Node helper(Node node) {
-        //exit of the recursion
-        if (node == null) return null;
+
         //check if it copy already return the new node
         if (map.containsKey(node)) return map.get(node);
 
