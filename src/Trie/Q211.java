89087f36aa8e4a797328d4c313c8f3b27f189fbe
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-04 23:05:53 -0700

    Update Q211.java

--- src/Trie/Q211.java
+++ src/Trie/Q211.java
@@ -1,6 +1,59 @@
 package Trie;
 
 public class Q211 {
+    class WordDictionary2 {
+        //data fields
+        class Node{
+            Node[] children = new Node[26];
+            boolean isWord = false;
+            //default constructor
+        }
+        Node root = new Node();
+
+        //default constructor
+        public void addWord(String word){
+            Node cur = root;
+            for(char c : word.toCharArray()){
+                if(cur.children[c - 'a'] == null) cur.children[c - 'a'] = new Node();
+                cur = cur.children[c - 'a'];//?
+            }
+            cur.isWord = true;
+        }
+
+        public boolean search(String word){
+            Node cur = root;
+            return helper(word, cur, 0);
+        }
+
+        //recursion
+        //index - index of the word
+        private boolean helper(String word, Node cur, int index){
+            //exit of the recursion
+            if(index == word.length()) return cur.isWord;
+
+            char c = word.charAt(index);
+            if( c != '.'){
+                if(cur.children[c - 'a'] == null) return false;
+                return helper(word, cur.children[c - 'a'], index + 1);
+            }
+
+            //start with '.'
+            for(Node child : cur.children){
+                if(child != null){
+                    if(helper(word, child, index + 1)) return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+    /**
+     * Your WordDictionary object will be instantiated and called as such:
+     * WordDictionary obj = new WordDictionary();
+     * obj.addWord(word);
+     * boolean param_2 = obj.search(word);
+     */
     class WordDictionary {
         //data fields
         class Node{
