080592999064c925f6c61f81e5ac4e0b4930922b
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-09 11:28:15 -0700

    Update Q169.java

--- src/arraryandstring/Q169.java
+++ src/arraryandstring/Q169.java
@@ -1,6 +1,32 @@
 package arraryandstring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q169 {
+    class Solution2 {
+        public int majorityElement(int[] nums) {
+            //transfer nums to hashmap
+            HashMap<Integer,Integer> map = new HashMap<>();
+
+            for(int i = 0; i < nums.length;i++){
+                if(map.containsKey(nums[i])){
+                    //map.get(i)
+                    map.put(nums[i], map.get(nums[i]) + 1);
+                }else{
+                    map.put(nums[i],1);
+                }
+            }
+
+            //check all the value
+            for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+                if(entry.getValue() > nums.length/2){
+                    return entry.getKey();
+                }
+            }
+            return -1;
+        }
+    }
     public int majorityElement(int[] nums) {
         int candidate = nums[0];
         int count = 0;
