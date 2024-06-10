5f85a846eac11c3cadbad44dadc587c54a224cc8
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-09 18:52:34 -0700

    Update Q128.java

--- src/hashmap/Q128.java
+++ src/hashmap/Q128.java
@@ -4,6 +4,32 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Q128 {
+    class Solution2 {
+        public int longestConsecutive(int[] nums) {
+            //错误写法：HashSet<int[]> set = new HashSet<>(nums);
+            //接受集合（如 ArrayList、LinkedList）
+            Set<Integer> set = new HashSet<>();
+            for(int n : nums){
+                set.add(n);
+            }
+
+            int res = 0;
+
+            for(int i = 0; i < nums.length; i++){
+                //check if it is the beginning 
+                if(!set.contains(nums[i]-1)){
+                    int cur = nums[i]; //the beginning cur
+                    int count = 1;
+                    while(set.contains(nums[i]+ 1)){
+                        count++;
+                        nums[i]++;
+                    }
+                    res = Math.max(res, count);
+                }
+            }
+            return res;
+        }
+    }
     class Solution {
         public int longestConsecutive(int[] nums) {
             //Hashset - without duplicate nums
