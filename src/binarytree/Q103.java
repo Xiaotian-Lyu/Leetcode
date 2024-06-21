adb05e43e41b083907bf26c7de5f1ab5ce34102f
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-21 15:45:58 -0700

    Update Q103.java

--- src/binarytree/Q103.java
+++ src/binarytree/Q103.java
@@ -6,6 +6,50 @@ import java.util.List;
 import java.util.Queue;
 
 public class Q103 {
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            boolean zigzag = false;// do the zigzag
+
+            //root null
+            if(root == null) return result;
+
+            //add root to queue
+            queue.add(root);
+
+            //keep poll form the queue
+            while(!queue.isEmpty()){
+                //check queue size for each level
+                int size = queue.size();
+                LinkedList<Integer> eachLevelNodes = new LinkedList<>();
+
+                for(int i = 0; i < size ; i++){
+                    TreeNode node = queue.poll();
+
+                    //check the next level add to queue
+                    if(node.left != null) queue.add(node.left);
+                    if(node.right != null) queue.add(node.right);
+
+                    //check the status of zigzag add to eachLevelNodes
+                    if(zigzag){ //true - add first to list - zigzag
+                        eachLevelNodes.addFirst(node.val);
+                    }else{//false - add last to list - normal
+                        eachLevelNodes.add(node.val);
+                    }
+                }
+                //each level end add to result
+                result.add(eachLevelNodes);
+
+                //change the status of zigzag
+                //错误写法：zigzag = - zigzag;
+                zigzag = !zigzag;
+            }
+
+            return result;
+
+        }
+    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         //queue feature: first in first out
         Queue<TreeNode> queue = new LinkedList<>();
