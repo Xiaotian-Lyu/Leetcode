783d7112478669b747fd78d1189b07bc750bd483
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-21 14:25:04 -0700

    update solution

--- src/binarytree/Q102.java
+++ src/binarytree/Q102.java
@@ -6,6 +6,34 @@ import java.util.List;
 import java.util.Queue;
 
 public class Q102 {
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+
+            //不能忘排除null
+            if(root == null) return result;
+
+            //put the root into queue
+            queue.add(root);
+
+            //pull out form the queue add to result
+            while(!queue.isEmpty()){
+                int size = queue.size();
+                List<Integer> eachLevel = new LinkedList<>();
+                for(int i = 0; i < size; i++){
+                    TreeNode node = queue.poll();
+                    //check next level of node add to queue
+                    if(node.left != null) queue.add(node.left);
+                    if(node.right != null) queue.add(node.right);
+                    eachLevel.add(node.val);
+                }
+                result.add(eachLevel);
+            }
+            return result;
+
+        }
+    }
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
 
--- src/binarytree/Q199.java
+++ src/binarytree/Q199.java
@@ -4,6 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Q199 {
+    class Solution {
+        List<Integer> result = new ArrayList<>();
+
+        public List<Integer> rightSideView(TreeNode root) {
+            helper(root,0);
+            return result;
+        }
+
+        //我需要一个function 可以传递level - root - right - left
+        public List<Integer> helper(TreeNode root, int level){
+            //遍历顺序root - right - left
+            if(root == null) return result;
+
+            //always put right side first
+            if(level == result.size()){
+                result.add(root.val);
+            }
+
+            //right side
+            helper(root.right, level + 1);
+            helper(root.left,  level + 1);
+
+            return result;
+        }
+    }
     //深度优先遍历
     public List<Integer> rightSideView(TreeNode root) {
         //each layer have only one node to be seen
