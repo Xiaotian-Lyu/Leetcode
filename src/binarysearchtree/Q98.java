d8e9b627c5e2329908ccdaf8965ddc6ec31ded07
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-24 10:47:00 -0700

    update BST

--- src/binarysearchtree/Q98.java
+++ src/binarysearchtree/Q98.java
@@ -3,6 +3,40 @@ package binarysearchtree;
 import binarytree.TreeNode;
 
 public class Q98 {
+    class Solution {
+        boolean result = true;
+        Integer preValue = null;
+        // root = [-2147483648]  -231 <= Node.val <= 231 - 1
+
+        public boolean isValidBST(TreeNode root) {
+            helper(root);
+            return result;
+        }
+        //inorder - DFS
+        public void helper(TreeNode root){
+            //exit of the recusion
+            if(root == null){
+                return;
+            }
+
+            //left
+            helper(root.left);
+
+            //root
+            if(preValue != null){
+                if(preValue >= root.val){
+                    result = false;
+                    return;
+                }
+            }
+            preValue = root.val;
+
+
+            //right
+            helper(root.right);
+
+        }
+    }
     //check the node.val : left < root < right
     public boolean isValidBST(binarytree.TreeNode root) {
         return helper(root,null,null);//错误写法helper(root,0,0)初始化是0的时候不能比较负数root = [0,null,-1]
--- src/binarytree/Q530.java
+++ src/binarytree/Q530.java
@@ -4,6 +4,54 @@ import com.sun.jdi.Value;
 import com.sun.source.tree.Tree;
 
 public class Q530 {
+    class Solution2 {
+        //dfs - inorder => small to big
+        int result = Integer.MAX_VALUE;
+        Integer preValue = null;
+        public int getMinimumDifference(TreeNode root) {
+            helper(root);
+            return result;
+        }
+        public void helper(TreeNode root){
+            //exit of the recursion
+            if(root == null) return;
+
+            //left
+            helper(root.left);
+
+            //root
+            if(preValue != null){
+                result = Math.min(result, root.val - preValue);
+            }
+            preValue = root.val;
+
+            //right
+            helper(root.right);
+        }
+    }
+    class Solution {
+        //dfs - inorder => small to big
+        int result = Integer.MAX_VALUE;
+        int preValue = Integer.MAX_VALUE;
+        public int getMinimumDifference(TreeNode root) {
+            helper(root);
+            return result;
+        }
+        public void helper(TreeNode root){
+            //exit of the recursion
+            if(root == null) return;
+
+            //left
+            helper(root.left);
+
+            //root
+            result = Math.min(result, Math.abs(root.val - preValue));
+            preValue = root.val;
+
+            //right
+            helper(root.right);
+        }
+    }
     int result = Integer.MAX_VALUE;// finding the minimum, so initialize max value
     int pre = Integer.MAX_VALUE;//a pre the store the last TreeNode
 
