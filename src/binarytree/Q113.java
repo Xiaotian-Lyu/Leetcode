7f11f0574b74d9ea826a0a4b74d54365ae0a5e61
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-26 13:07:39 -0700

    Update Q113.java

--- src/binarytree/Q113.java
+++ src/binarytree/Q113.java
@@ -5,6 +5,42 @@ import java.util.List;
 
 public class Q113 {
     /**
+     * 不断更替target value 并 backtracking
+     */
+    class Solution2 {
+        List<List<Integer>> result = new ArrayList<>();
+        public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+            List<Integer> path =  new ArrayList<>();
+            DFS(root, targetSum, path);
+            return result;
+        }
+        //DFS - pre - order
+        public void DFS(TreeNode root, int targetSum, List<Integer> path){ //path 是引用变量 只有一个值 需要 backtracking
+            //exit of the recursion
+            if(root == null) return;
+            if(root.left == null && root.right == null && root.val == targetSum){
+                path.add(root.val);//不能漏
+                //错误写法：result.add(path);
+                result.add(new ArrayList<>(path));
+                path.remove(path.size() - 1); //不能漏 可以和最后一行合并 那就要把下面的内容写到else 里面去
+                return;
+            }
+            path.add(root.val);
+            //left
+            DFS(root.left, targetSum - root.val, path);
+
+            //right
+            DFS(root.right, targetSum - root.val, path);
+
+            //backtracking
+            //错误写法：path.setLength(path.size() - 1);//setLength 方法通常用于字符串操作，特别是在 Java 中的 StringBuilder 类中
+            path.remove(path.size() - 1);
+            return;
+
+        }
+
+    }
+    /**
      * 方法一：从root 往下加值
      */
     class Solution {
