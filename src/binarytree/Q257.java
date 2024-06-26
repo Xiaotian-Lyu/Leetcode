20c90f52164d1349c51a94e96eb28f7533fe6655
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-26 15:13:07 -0700

    Update Q257.java

--- src/binarytree/Q257.java
+++ src/binarytree/Q257.java
@@ -4,7 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Q257 {
-    class Solution {
+    /**
+     *涉及到String 的拼接的尽量用StringBuilder
+     * 解决方案 1 中字符串拼接的高时间复杂度主要是由于每次拼接操作都会创建新的字符串对象。
+     * 解决方案 2 使用 StringBuilder 更加高效，避免了频繁创建新字符串，具有更好的性能和更低的时间复杂度。
+     */
+    class Solution2 {
+        List<String> result = new ArrayList<>();
+
+        public List<String> binaryTreePaths(TreeNode root) {
+            StringBuilder builder = new StringBuilder();
+            DFS(root, builder);
+            return result;
+        }
+        public void DFS(TreeNode root , StringBuilder builder){
+            //pre order
+            //exit of the recursion
+            if(root == null) return;
+
+            int length = builder.length();
+
+            //reach to the leaf
+            if(root.left == null && root.right == null){
+                builder.append(root.val);//到叶子结点就不需要 append("->");
+                result.add(builder.toString());
+
+            }else{
+                builder.append(root.val);//没有到叶子结点也要加上去
+                builder.append("->");//不是叶子结点就说明 左边或者右边一定有 node 所以一定加
+                //left
+                if(root.left != null) DFS(root.left, builder);
+                //right
+                if(root.right != null) DFS(root.right, builder);
+            }
+
+            //backtracking
+            builder.setLength(length);
+
+        }
+    }
+
+    /**
+     * 这个用string直接add 时间会慢
+     */
+    class Solution1 {
         List<String> result = new ArrayList<>();
 
         public List<String> binaryTreePaths(TreeNode root) {
