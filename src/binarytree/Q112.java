e7b7455f847943be9d605e6dd00b07db282555b7
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-27 20:43:45 -0700

    Update Q112.java

--- src/binarytree/Q112.java
+++ src/binarytree/Q112.java
@@ -1,6 +1,29 @@
 package binarytree;
 
 public class Q112 {
+    class Solution2 {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            int sum = 0;
+            return DFS(root, targetSum, sum);
+        }
+
+        //DSF root- left - right preorder
+        public boolean DFS(TreeNode root, int targetSum, int sum){
+            if(root == null) return false;
+
+            if(root.left == null && root.right == null){
+                sum += root.val;
+                if(sum == targetSum){
+                    return true;//是这一层的返回 true；不是整个function 返回的是 true
+                }//else{
+                //     return false; 可以不用return false 因为遍历到下一层 判断到root == null 后直接return false
+                // }
+            }
+
+            return DFS(root.left, targetSum, sum + root.val) || DFS(root.right, targetSum, sum + root.val);
+
+        }
+    }
     class Solution {
         public boolean hasPathSum(TreeNode root, int targetSum) {
             // if(root == null) return false; 这句可以不写
