4260a0b46fccd1613df465f147adab9b02256c5e
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-28 16:13:38 -0700

    update 232/155

--- src/stack/Q155.java
+++ src/stack/Q155.java
@@ -1,6 +1,59 @@
 package stack;
 
 public class Q155 {
+    class MinStack2 {
+        class Node{
+            Node next;
+            int min;
+            int val;
+            //constructer
+            public Node(int val, int min, Node next){
+                this.val = val;
+                this.min = min;
+                this.next = next;
+            }
+        }
+
+        //A head manupulate
+        Node head; //声明了Node 但是没有新建 说明这个head == null
+
+        public MinStack2() {
+            //空参构造器 如果没有往里push 值。它的head 就是 null
+        }
+
+        public void push(int val) {
+            //check the head first
+            if(head == null){
+                this.head = new Node(val, val, null);
+            }else{
+                //错误写法：this.head = new Node(val, Math.min(head.val, val), head);
+                this.head = new Node(val, Math.min(head.min, val), head);
+            }
+
+        }
+
+        public void pop() {
+            this.head = head.next;
+
+        }
+
+        public int top() {
+            return head.val;
+        }
+
+        public int getMin() {
+            return head.min;
+        }
+    }
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.push(val);
+     * obj.pop();
+     * int param_3 = obj.top();
+     * int param_4 = obj.getMin();
+     */
     class MinStack {
 
         class Node{
--- src/stack/Q232.java
+++ src/stack/Q232.java
@@ -3,6 +3,49 @@ package stack;
 import java.util.Stack;
 
 public class Q232 {
+    class MyQueue {
+
+        Stack<Integer> inStack;
+        Stack<Integer> outStack;
+
+        public MyQueue() {
+            //use two stack
+            //错误 Stack<Integer> inStack = new Stack<>();
+            //错误 Stack<Integer> outStack = new Stack<>();
+            this.inStack =  new Stack<>();
+            this.outStack =  new Stack<>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop() {
+            if(outStack.isEmpty()){
+                //put instack into outstack
+                while(!inStack.isEmpty()){
+                    outStack.push(inStack.pop());
+                }
+            }
+            return outStack.pop();
+        }
+
+        public int peek() {
+            if(outStack.isEmpty()){
+                //put instack into outstack
+                while(!inStack.isEmpty()){
+                    outStack.push(inStack.pop());
+                }
+            }
+            return outStack.peek();
+        }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+
+        }
+    }
+
     Stack<Integer> inStack = new Stack<Integer>();//为什么要是Integer
     Stack<Integer> outStack = new Stack<Integer>();
 
