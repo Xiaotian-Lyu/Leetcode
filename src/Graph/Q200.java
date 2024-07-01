1475015f419ab7f58dfa0b12dbc388cae38bc1c6
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-06-30 23:05:46 -0700

    Update Q200.java

--- src/Graph/Q200.java
+++ src/Graph/Q200.java
@@ -1,6 +1,40 @@
 package Graph;
 
 public class Q200 {
+    class Solution {
+        public int numIslands(char[][] grid) {
+            int m = grid.length, n = grid[0].length;
+            int result = 0;
+            for(int i = 0; i < m ; i++){
+                for(int j = 0; j < n ; j++){
+                    //错误写法：if(grid[i][j] == 1){
+                    if(grid[i][j] == '1'){
+                        result++;
+                        DFS(grid, i, j, m, n);
+                    }
+                }
+            }
+
+            return result;
+
+        }
+
+        int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+
+        public void DFS(char[][] grid, int i, int j, int m, int n){
+            //exit of the recursion
+            //错误写法：if(i < 0 || j < 0 || i > m || j > n || grid[i][j] == '0') return;
+            if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') return;
+
+            //change the 1 to 0
+            grid[i][j] = '0';
+
+            for(int[] direction : directions){
+                DFS(grid, i + direction[0], j + direction[1], m, n);
+            }
+
+        }
+    }
     public int numIslands(char[][] grid) {
         //Initialize variables
         int res = 0, m = grid.length, n = grid[0].length;
