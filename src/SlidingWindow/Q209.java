02348577b3ffca41a7a94d56051f3e4f539def3e
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-07-09 16:37:45 -0700

    update tire

--- src/SlidingWindow/Q209.java
+++ src/SlidingWindow/Q209.java
@@ -1,6 +1,26 @@
 package SlidingWindow;
 
 public class Q209 {
+    class Solution2 {
+        public int minSubArrayLen(int target, int[] nums) {
+            int start = 0, end = 0, sum = 0;
+            int result = Integer.MAX_VALUE;
+            while(end < nums.length){
+                sum += nums[end];
+                while(sum >= target){
+                    result = Math.min(result, end - start + 1);
+                    //start upadte
+                    sum -= nums[start];
+                    start++;
+                }
+                end++;
+            }
+
+            //注意 输出结果的时候要检查一下，最后的result 如果还是最大值的话，那就说明没有找到符合target的值
+            return result == Integer.MAX_VALUE ? 0:result;
+
+        }
+    }
     class Solution {
         public int minSubArrayLen(int target, int[] nums) {
             //错误写法：res = Integer.MaxValue;
--- src/Trie/Q211.java
+++ src/Trie/Q211.java
@@ -1,6 +1,66 @@
 package Trie;
 
 public class Q211 {
+    class WordDictionary3 {
+        Node root = new Node();
+
+        class Node{
+            //attributes 错误写法
+            Node[] children = new Node[26];
+            boolean isWord = false;
+            //defalut constructor
+        }
+        public WordDictionary3() {
+            //defalut constructor
+        }
+
+        public void addWord(String word) {
+            Node cur = root;
+            for(char c : word.toCharArray()){
+                if(cur.children[c - 'a'] == null) cur.children[c - 'a'] = new Node();
+                cur = cur.children[c - 'a'];
+            }
+            cur.isWord = true;
+        }
+
+        public boolean search(String word) {
+            //create a function to do the recursion
+            Node cur = root;//在 search 方法中，cur 应该初始化为 root，而不是一个新的 Node 对象。
+            return helper(word, 0, cur);
+        }
+
+        public boolean helper(String word, int index ,Node cur){
+            //exit of the recursion
+            if(index == word.length()) return cur.isWord;
+
+            char c = word.charAt(index);
+
+            //if not '.'
+            if(c != '.'){
+                if(cur.children[c - 'a'] == null) return false;
+                //find the char //错 if(helper(word, index + 1, cur.children[c - 'a'])) return true;
+                return helper(word, index + 1, cur.children[c - 'a']);//直接return 调用helper 让下一层来返回值就行
+            }
+
+
+            //if is the '.'
+            for(Node children : cur.children){
+                if(children != null){
+                    if(helper(word, index + 1, children)) return true;//这里要判断下一层是true 才能返回 true 因为‘.'可以是任何位置
+                }
+            }
+
+            return false;
+
+        }
+    }
+
+    /**
+     * Your WordDictionary object will be instantiated and called as such:
+     * WordDictionary obj = new WordDictionary();
+     * obj.addWord(word);
+     * boolean param_2 = obj.search(word);
+     */
     class WordDictionary2 {
         //data fields
         class Node{
