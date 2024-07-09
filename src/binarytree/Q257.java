c1535b1d8dff422e2915e5623cc06e885a2ea689
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-07-08 21:44:08 -0700

    Update Q257.java

--- src/binarytree/Q257.java
+++ src/binarytree/Q257.java
@@ -5,6 +5,63 @@ import java.util.List;
 
 public class Q257 {
     /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        List<String> result = new ArrayList<>();
+
+        public List<String> binaryTreePaths(TreeNode root) {
+            DFS(root, new StringBuilder());
+            return result;
+        }
+
+        public void DFS(TreeNode root, StringBuilder builder){
+            if(root == null) return;
+
+            //在一开始就记录builder的长度 以便回溯
+            int length = builder.length();
+
+            //exit of the recursion
+            if(root.left == null && root.right == null){
+                builder.append(root.val);
+                result.add(builder.toString());
+                //backTraking
+                //错误写法builder.setLength(builder.length() - 1); 因为builder中有这个“->“ 所以只减去一个index 是不够的
+                builder.setLength(length);
+                return;
+            }
+
+            //root
+            builder.append(root.val).append("->");
+
+            //left
+            if(root.left != null){
+                DFS(root.left, builder);
+            }
+
+            //right
+            if(root.right != null){
+                DFS(root.right, builder);
+            }
+
+            //backtracking
+            //错误写法builder.setLength(builder.length() - 1); 因为builder中有这个“->“ 所以只减去一个index 是不够的
+            builder.setLength(length);
+        }
+    }
+    /**
      *涉及到String 的拼接的尽量用StringBuilder
      * 解决方案 1 中字符串拼接的高时间复杂度主要是由于每次拼接操作都会创建新的字符串对象。
      * 解决方案 2 使用 StringBuilder 更加高效，避免了频繁创建新字符串，具有更好的性能和更低的时间复杂度。
