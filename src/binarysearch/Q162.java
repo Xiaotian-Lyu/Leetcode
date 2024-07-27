b2fb3df66507bd089a9702ea6bb10c501b419539
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-07-27 12:20:23 -0700

    Update Q162.java

--- src/binarysearch/Q162.java
+++ src/binarysearch/Q162.java
@@ -1,6 +1,25 @@
 package binarysearch;
 
 public class Q162 {
+    class Solution {
+        public int findPeakElement(int[] nums) {
+            int left = 0, right = nums.length - 2;////注意⚠️这里的i < nums.length - 1，i的最大值是i = nums.length - 2
+            //为了保证nums[i + 1]不越界
+
+            while(left <= right){
+                int mid = left + (right - left)/2;
+
+                if(nums[mid] < nums[mid + 1]){
+                    left = mid + 1;
+                }else if(nums[mid] > nums[mid + 1]){
+                    right = mid - 1;
+                }
+
+            }
+
+            return left;
+        }
+    }
     public int findPeakElement(int[] nums) {
         //binary search to compare mid and mid+1
         //不断缩小左边界 同时不断缩小右边界 直到left = right 这个时候是peak
