ce38d8940da99af8be3c57c85f20fdb503350991
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-08-13 16:55:44 -0700

    Update Q295.java

--- src/heap/Q295.java
+++ src/heap/Q295.java
@@ -3,6 +3,45 @@ package heap;
 import java.util.PriorityQueue;
 
 public class Q295 {
+    class MedianFinder2 {
+        private PriorityQueue<Integer> maxHeap; // 保存较小的一半
+        private PriorityQueue<Integer> minHeap; // 保存较大的一半
+
+        public MedianFinder2() {
+            maxHeap = new PriorityQueue<>((a, b) -> b - a);
+            minHeap = new PriorityQueue<>();
+        }
+
+        public void addNum(int num) {
+            //add to the minHeap - 先加到minheap 就说明minheap 奇数多一个数字 - 保证minheap 的size 比较大
+            //如果是先加到maxheap 就说明maxheap 奇数多一个数字 - 保证maxheap 的size 比较大
+            minHeap.add(num);
+            maxHeap.add(minHeap.poll());
+
+            if(maxHeap.size() > minHeap.size()){
+                minHeap.add(maxHeap.poll());
+            }
+        }
+
+        public double findMedian() {
+            //even
+            if(maxHeap.size() == minHeap.size()){
+                return (maxHeap.peek() + minHeap.peek()) / 2.0;
+            }else{
+                //odd
+                return minHeap.peek();
+            }
+
+        }
+    }
+
+
+    /**
+     * Your MedianFinder object will be instantiated and called as such:
+     * MedianFinder obj = new MedianFinder();
+     * obj.addNum(num);
+     * double param_2 = obj.findMedian();
+     */
     class MedianFinder {
         //two heap
         PriorityQueue<Integer> maxHeap;//keep the half small nums
