c12c6dc97bbafcfbb8be6492ef7fb951e98e804c
Xiaotian-Lyu <lyu.xiao@northeastern.edu>
2024-08-21 14:37:10 -0700

    Update Q416.java

--- src/dp/Q416.java
+++ src/dp/Q416.java
@@ -1,6 +1,43 @@
 package dp;
 
 public class Q416 {
+    class Solution2 {
+        public boolean canPartition(int[] nums) {
+            int m = nums.length;
+            int sum = 0;
+            for(int num : nums){
+                sum += num;
+            }
+            if(sum % 2 == 1) return false;
+            int target = sum / 2;
+
+            boolean[][] dp = new boolean[m + 1][target + 1];
+            //m = how many nums; n = target number
+
+            //initial the dp
+            for(int i = 0; i < m + 1; i++){
+                dp[i][0] = true;//dp[0][0] = true
+            }
+
+            for(int j = 1; j < target + 1; j++){
+                dp[0][j] = false;
+            }
+
+            for(int i = 1; i < m + 1; i++){
+                for(int j = 1; j < target + 1; j++){
+                    if(nums[i - 1] <= j){//这里的i代表长度
+                        //two options
+                        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i-1]];
+                    }else{
+                        //one option
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+
+            return dp[nums.length][target];
+        }
+    }
     class Solution {
         public boolean canPartition(int[] nums) {
             //check sum of nums ：even or odd
